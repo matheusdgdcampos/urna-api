3a95f5b586d34f762b43515fb5405eeb26d14809
Teuuz1994 <matheus.rj.ig@gmail.com>
2021-01-23 19:18:09 -0300

    feat: middleware of catch errors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const cors = require('cors')
 const app = express()
 const port = 3333
 
+const catchErrorMiddleware = require('./middleware/catchError')
+
 app.use(express.json())
 app.use(cors())
+app.use(catchErrorMiddleware)
 
 app.listen(port, () => console.info('[INFO] Server is running...'))
