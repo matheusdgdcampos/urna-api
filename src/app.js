b040bd35f03190c9a426156566be8efe52d86084
Teuuz1994 <matheus.rj.ig@gmail.com>
2021-03-04 00:10:06 -0300

    fix: added cross-env lib

--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
-require('dotenv').config()
-require('./config/database')
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+if (process.env.NODE === 'development') {
+  require('dotenv').config()
+  require('./config/database')
+  const express = require('express')
+  const cors = require('cors')
+  const path = require('path')
 
-const app = express()
-const port = 3333
+  const app = express()
+  const port = 3333
 
-const catchErrorMiddleware = require('./middleware/catchError')
-const routes = require('./routes')
+  const catchErrorMiddleware = require('./middleware/catchError')
+  const routes = require('./routes')
 
-app.use(cors())
-app.use(express.json())
-app.use(catchErrorMiddleware)
-app.use('/picture', express.static(path.resolve(__dirname, '..', 'uploads')))
-app.use(routes)
+  app.use(cors())
+  app.use(express.json())
+  app.use(catchErrorMiddleware)
+  app.use('/picture', express.static(path.resolve(__dirname, '..', 'uploads')))
+  app.use(routes)
 
-app.listen(port, () => console.info('[INFO] Server is running...'))
+  app.listen(port, () => console.info('[INFO] Server is running...'))
+} else {
+  require('./config/database')
+  const express = require('express')
+  const cors = require('cors')
+  const path = require('path')
+
+  const app = express()
+  const port = 3333
+
+  const catchErrorMiddleware = require('./middleware/catchError')
+  const routes = require('./routes')
+
+  app.use(cors())
+  app.use(express.json())
+  app.use(catchErrorMiddleware)
+  app.use('/picture', express.static(path.resolve(__dirname, '..', 'uploads')))
+  app.use(routes)
+
+  app.listen(port, () => console.info('[INFO] Server is running...'))
+}
