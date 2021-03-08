ff325f2ecb98227a3055ebb54bdf123b118f2109
Teuuz1994 <matheus.rj.ig@gmail.com>
2021-03-07 21:17:20 -0300

    fix: route update candidate

--- src/routes/candidates.routes.js
+++ src/routes/candidates.routes.js
@@ -20,6 +20,6 @@ candidatesRoutes.patch(
   candidatesController.avatar
 )
 candidatesRoutes.get('/:_id/show', candidatesController.show)
-candidatesRoutes.post('/candidato/edit/:_id', candidatesController.update)
+candidatesRoutes.put('/candidato/edit/:_id', candidatesController.update)
 
 module.exports = candidatesRoutes
